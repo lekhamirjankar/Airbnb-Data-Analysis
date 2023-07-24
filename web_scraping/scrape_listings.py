4bf281a96793aeceeaf9f70cce7db470ff9c0d48
Lekha Mirjankar <65814673+lekhamirjankar@users.noreply.github.com>
2023-07-24 21:12:04 +0530

    - store all details in respective lists - create DataFrame - convert to csv

--- web_scraping/scrape_listings.py
+++ web_scraping/scrape_listings.py
@@ -1,6 +1,17 @@
 from bs4 import BeautifulSoup
 #import requests library
 import requests
+#import pandas library
+import pandas as pd
+
+Link = []
+Title = []
+Subtitle =[]
+Desc = []
+Dates = []
+Price = []
+Reviews = []
+
 #the website URL
 url_link = "https://www.airbnb.co.in/s/India/homes?adults=1&place_id=ChIJkbeSa_BfYzARphNChaFPjNc&refinement_paths%5B%5D=%2Fhomes"
 
@@ -18,11 +29,9 @@ max = max(counter)
 
 # loop through each page
 for i in range(1,max):
-    np=doc.find("a", class_ = "l1ovpqvx c1ytbx3a dir dir-ltr").get("href")
 
-    cnp = "https://www.airbnb.co.in" + np
-
-    url_link = cnp
+    nextpage = "https://www.airbnb.co.in" + doc.find("a", class_ = "l1ovpqvx c1ytbx3a dir dir-ltr").get("href")    
+    url_link = nextpage
     result = requests.get(url_link, verify=False)
     doc = BeautifulSoup(result.text, "html.parser")
 
@@ -31,26 +40,40 @@ for i in range(1,max):
         try:
 
             #get link
-            Link =  item.select('a')[0]['href']
-            print("https://www.airbnb.co.in" + Link)
+            getlink =  item.select('a')[0]['href']
+            link = "https://www.airbnb.co.in" + getlink
+            Link.append(link)
             
             # get the title
-            print(item.select('[data-testid=listing-card-title]')[0].get_text())
+            title = item.select('[data-testid=listing-card-title]')[0].get_text()
+            Title.append(title)
 
             # get the sub title
-            print(item.select('[data-testid=listing-card-subtitle]')[0].get_text())
+            subtitle = item.select('[data-testid=listing-card-subtitle]')[0].get_text()
+            Subtitle.append(subtitle)
 
             # get the desc
-            print(item.select('[data-testid=listing-card-subtitle]')[1].get_text())
+            desc = item.select('[data-testid=listing-card-subtitle]')[1].get_text()
+            Desc.append(desc)
 
             # get dates
-            print(item.select('[data-testid=listing-card-subtitle]')[2].get_text())
+            dates = item.select('[data-testid=listing-card-subtitle]')[2].get_text()
+            Dates.append(dates)
 
             # get price
-            print(item.select('[data-testid=card-container] span span')[4].get_text()) 
+            price = item.select('[data-testid=card-container] span span')[4].get_text()
+            Price.append(price)
 
             # get reveiws
-            print(item.select('[aria-label]')[0].get_text())
+            reviews = item.select('[aria-label]')[0].get_text()
+            Reviews.append(reviews)
+            
+            # create dataframe
+            df = {"Name":Title, "Sub-title":Subtitle, "Details":Desc, "Dates":Dates, "Price":Price, "Reviews":Reviews, "Link":Link}
+            listdf=pd.DataFrame(df)
+
+            # convert to csv
+            listdf.to_csv("airbnb_data.csv")
 
         except Exception as e:
             print('Exception occured: ', e)
